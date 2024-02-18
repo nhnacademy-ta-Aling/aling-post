cfca8fb33f16bf56da7c159b3605d825e02b90bc
devel0624 <devel062@gmail.com>
2024-02-19 06:30:51 +0900

    [Feat] Spring Cloud OpenFeign Client 적용
    - 실제 서비스 코드에는 아직 포함하지 않고 의존성 적용만 하였습니다.
    - Sample 컨트롤러를 이용하여 유레카 서버의 actuator 를 확인해볼 수 있습니다.

---
+++ src/main/java/kr/aling/post/config/EurekaConfig.java
@@ -0,0 +1,20 @@
+package kr.aling.post.config;
+
+import kr.aling.post.common.feign.client.FeignClientBase;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Eureka 와 관련된 설정이 모여있는 Java Config 입니다.
+ * 본래 PostApplication.class 상단에서 @EnableFeignClients 적용시 별다른 설정이 필요없지만
+ * 이와 같이 분리할 경우 FeignClient 들의 위치를 명시해주어야 읽을 수 있습니다.
+ *
+ * @author : 이성준
+ * @since : 1.0
+ */
+@Configuration
+@EnableEurekaClient
+@EnableFeignClients(basePackageClasses = FeignClientBase.class)
+public class EurekaConfig {
+}
