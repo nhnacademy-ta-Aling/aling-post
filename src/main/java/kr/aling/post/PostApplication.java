3cba7f5aeebb51e6b1fdb6c2ad1cd4f080e53e74
Gyeongseo Bak <rudtj2966@gmail.com>
2024-02-19 10:01:07 +0900

    [Feat] : DBCP2 설정
    - custom properties
    - DBCP2 Bean

--- src/main/java/kr/aling/post/PostApplication.java
+++ src/main/java/kr/aling/post/PostApplication.java
@@ -2,6 +2,7 @@ package kr.aling.post;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 /**
  * PostApplication.
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 1.0
  **/
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class PostApplication {
 
     public static void main(String[] args) {
