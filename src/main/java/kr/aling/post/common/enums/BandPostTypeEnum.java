fa1f02a16433b7ffe14d0f491dcc449c148232dd
hobby3530 <jungy3530@gmail.com>
2024-02-26 17:24:38 +0900

    [Feat] 그룹 관련 enum 추가
    - 그룹 게시글 분류 enum
    - 그룹 회원 권한 enum

---
+++ src/main/java/kr/aling/post/common/enums/BandPostTypeEnum.java
@@ -0,0 +1,19 @@
+package kr.aling.post.common.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 그룹 게시글 분류 Enum.
+ *
+ * @author : 정유진
+ * @since : 1.0
+ **/
+@Getter
+@RequiredArgsConstructor
+public enum BandPostTypeEnum {
+    DEFAULT("default");
+
+    private final String bandPostTypeName;
+}
+
