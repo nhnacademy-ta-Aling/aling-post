ac52d5b327e3e0545b1df97a2c6371b63aa135f7
hobby3530 <jungy3530@gmail.com>
2024-02-26 17:23:47 +0900

    [Feat] 그룹 게시글 분류 api 추가 - repository
    - 조회 및 생성 메서드 추가

---
+++ src/main/java/kr/aling/post/bandposttype/repository/BandPostTypeManageRepository.java
@@ -0,0 +1,13 @@
+package kr.aling.post.bandposttype.repository;
+
+import kr.aling.post.bandposttype.entity.BandPostType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Some description here.
+ *
+ * @author : 정유진
+ * @since : 1.0
+ **/
+public interface BandPostTypeManageRepository extends JpaRepository<BandPostType, Long> {
+}
---
+++ src/main/java/kr/aling/post/bandposttype/repository/BandPostTypeReadRepositoryCustom.java
@@ -0,0 +1,30 @@
+package kr.aling.post.bandposttype.repository;
+
+import java.util.List;
+import kr.aling.post.bandposttype.dto.response.GetBandPostTypeResponseDto;
+
+/**
+ * 그룹 게시글 분류 repository custom.
+ *
+ * @author : 정유진
+ * @since : 1.0
+ **/
+public interface BandPostTypeReadRepositoryCustom {
+
+    /**
+     * 그룹 게시글 분류 존재(중복) 여부를 확인 하는 메서드입니다.<br>
+     *
+     * @param name 그룹 게시글 분류 이름
+     * @param bandNo 그룹 번호
+     * @return 존재(중복) 여부
+     */
+    boolean existsByNameAndBandNo(String name, Long bandNo);
+
+    /**
+     * 특정 그룹의 그룹 게시글 분류 리스트를 조회하는 메서드입니다.
+     *
+     * @param bandNo 그룹 번호
+     * @return 그룹 게시글 분류 dto 리스트
+     */
+    List<GetBandPostTypeResponseDto> getBandPostTypeListByBandNo(Long bandNo);
+}
