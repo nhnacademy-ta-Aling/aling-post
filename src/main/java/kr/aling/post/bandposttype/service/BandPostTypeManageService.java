212e28449a449e74fe47fa5b9ecaf13d2789cac5
hobby3530 <jungy3530@gmail.com>
2024-02-27 21:05:57 +0900

    [Refactor] 기본 그룹 게시글 타입 생성 api 삭제
    - 기룹 게시글 타입 생성 api 로 대체

--- src/main/java/kr/aling/post/bandposttype/service/BandPostTypeManageService.java
+++ src/main/java/kr/aling/post/bandposttype/service/BandPostTypeManageService.java
@@ -12,13 +12,6 @@ import kr.aling.post.bandposttype.dto.request.CreateBandPostTypeRequestDto;
 public interface BandPostTypeManageService {
 
     /**
-     * 기본 게시글 분류 타입을 생성 하는 메서드 입니다.
-     *
-     * @param requestDto 기본 게시글 분류 타입 생성에 필요한 정보를 담은 dto
-     */
-    void makeDefaultType(CreateBandPostTypeDefaultRequestDto requestDto);
-
-    /**
      * 게시글 분류 타입을 생성 하는 메서드 입니다.
      *
      * @param requestDto 기본 게시글 분류 타입 생성에 필요한 정보를 담은 dto
--- src/main/java/kr/aling/post/bandposttype/service/impl/BandPostTypeManageServiceImpl.java
+++ src/main/java/kr/aling/post/bandposttype/service/impl/BandPostTypeManageServiceImpl.java
@@ -1,6 +1,5 @@
 package kr.aling.post.bandposttype.service.impl;
 
-import kr.aling.post.bandposttype.dto.request.CreateBandPostTypeDefaultRequestDto;
 import kr.aling.post.bandposttype.dto.request.CreateBandPostTypeRequestDto;
 import kr.aling.post.bandposttype.entity.BandPostType;
 import kr.aling.post.bandposttype.exception.BandPostTypeAlreadyExistsException;
@@ -8,7 +7,6 @@ import kr.aling.post.bandposttype.repository.BandPostTypeManageRepository;
 import kr.aling.post.bandposttype.repository.BandPostTypeReadRepository;
 import kr.aling.post.bandposttype.service.BandPostTypeManageService;
 import kr.aling.post.common.annotation.ManageService;
-import kr.aling.post.common.enums.BandPostTypeEnum;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -27,26 +25,7 @@ public class BandPostTypeManageServiceImpl implements BandPostTypeManageService 
     /**
      * {@inheritDoc}
      *
-     * @param requestDto 기본 게시글 분류 타입 생성에 필요한 정보를 담은 dto
-     * @throws BandPostTypeAlreadyExistsException 기본 게시글 분류가 그룹 내에 이미 존재 하는 경우
-     */
-    public void makeDefaultType(CreateBandPostTypeDefaultRequestDto requestDto) {
-        if (bandPostTypeReadRepository.existsByNameAndBandNo(BandPostTypeEnum.DEFAULT.getBandPostTypeName(),
-                requestDto.getBandNo())) {
-            throw new BandPostTypeAlreadyExistsException();
-        }
-
-        bandPostTypeManageRepository.save(
-                BandPostType.builder()
-                        .bandNo(requestDto.getBandNo())
-                        .name("default")
-                        .build());
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @param requestDto 기본 게시글 분류 타입 생성에 필요한 정보를 담은 dto
+     * @param requestDto 게시글 분류 타입 생성에 필요한 정보를 담은 dto
      * @throws BandPostTypeAlreadyExistsException 기본 게시글 분류가 그룹 내에 이미 존재 하는 경우
      */
     @Override
