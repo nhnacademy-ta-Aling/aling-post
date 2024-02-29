52fb354315d8f3f9280c1a8ed5d9a99b35c98ad0
Gyeongseo Bak <rudtj2966@gmail.com>
2024-02-29 15:02:35 +0900

    [Feat] : Dto
    - 게시글 최종 응답 Dto

--- src/main/java/kr/aling/post/bandpost/dto/response/GetBandResponseDto.java
+++ src/main/java/kr/aling/post/bandpost/dto/response/GetBandResponseDto.java
@@ -5,7 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
- * Some description here.
+ * 하나의 게시글 정보 응답 Dto. <br>
+ * <ol>
+ *     <li>작성자 정보</li>
+ *     <li>게시글 정보</li>
+ * </ol>
  *
  * @author 박경서
  * @since 1.0
