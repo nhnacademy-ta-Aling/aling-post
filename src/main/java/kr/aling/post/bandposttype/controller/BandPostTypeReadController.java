bd0901cd0c093d21fed13198dc8082cae65a7ddb
hobby3530 <jungy3530@gmail.com>
2024-03-07 23:26:19 +0900

    [Feat] 그룹 게시글 분류 수정, 삭제 api 관련 Controller 구현

--- src/main/java/kr/aling/post/bandposttype/controller/BandPostTypeReadController.java
+++ src/main/java/kr/aling/post/bandposttype/controller/BandPostTypeReadController.java
@@ -7,19 +7,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 그룹 게시물 분류를 조회하기 위한 Rest Controller.
+ * 그룹 게시물 분류를 조회하기 위한 Rest Controller.<br>
+ * 모든 그룹 게시글 분류에 관한 요청은 User 서버의 Band 를 거쳐 와야 합니다.
  *
  * @author 정유진
  * @since 1.0
  **/
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/bands/{bandNo}/band-post-types")
+@RequestMapping("/api/v1/band-post-types")
 public class BandPostTypeReadController {
 
     private final BandPostTypeReadService bandPostTypeReadService;
@@ -27,11 +28,11 @@ public class BandPostTypeReadController {
     /**
      * 그룹 게시물 분류 리스트를 조회 하기 위한 메서드입니다. <br> 특정 그룹의 그룹 게시물 분류 리스트를 조회 합니다.
      *
-     * @param bandNo 그룹 번호
      * @return 200 ok. 그룹 게시물 분류 정보 dto 리스트
+     * @pxaram bandNo 그룹 번호
      */
     @GetMapping
-    public ResponseEntity<List<GetBandPostTypeResponseDto>> bandPostTypeList(@PathVariable("bandNo") Long bandNo) {
+    public ResponseEntity<List<GetBandPostTypeResponseDto>> bandPostTypeList(@RequestParam("bandNo") Long bandNo) {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(bandPostTypeReadService.getBandPostTypeList(bandNo));
