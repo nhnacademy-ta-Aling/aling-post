b1290d2cd9332ecc0a51f967ffd9126752bc819d
hobby3530 <jungy3530@gmail.com>
2024-03-07 23:26:42 +0900

    [Feat] 그룹 게시글 분류 수정, 삭제 api 관련 Service 구현

--- src/main/java/kr/aling/post/bandposttype/service/BandPostTypeManageService.java
+++ src/main/java/kr/aling/post/bandposttype/service/BandPostTypeManageService.java
@@ -1,7 +1,7 @@
 package kr.aling.post.bandposttype.service;
 
-import kr.aling.post.bandposttype.dto.request.CreateBandPostTypeDefaultRequestDto;
 import kr.aling.post.bandposttype.dto.request.CreateBandPostTypeRequestDto;
+import kr.aling.post.bandposttype.dto.request.ModifyBandPostTypeRequestDto;
 
 /**
  * 그룹 게시글 분류 관리 service.
@@ -12,9 +12,24 @@ import kr.aling.post.bandposttype.dto.request.CreateBandPostTypeRequestDto;
 public interface BandPostTypeManageService {
 
     /**
-     * 게시글 분류 타입을 생성 하는 메서드 입니다.
+     * 그룹 게시글 분류를 생성 하는 메서드 입니다.
      *
-     * @param requestDto 기본 게시글 분류 타입 생성에 필요한 정보를 담은 dto
+     * @param requestDto 그룹 게시글 분류 생성에 필요한 정보를 담은 dto
      */
     void makeBandPostType(CreateBandPostTypeRequestDto requestDto);
+
+    /**
+     * 그룹 게시글 분류 수정을 위한 메서드입니다.
+     *
+     * @param postTypeNo 수정할 그룹 게시글 분류 번호
+     * @param requestDto 수정할 그룹 게시글 분류 정보 dto
+     */
+    void updateBandPostType(Long postTypeNo, ModifyBandPostTypeRequestDto requestDto);
+
+    /**
+     * 그룹 게시글 분류 삭제를 위한 메서드입니다.<br>
+     *
+     * @param postTypeNo 그룹 게시글 분류 번호
+     */
+    void deleteBandPostType(Long postTypeNo);
 }
--- src/main/java/kr/aling/post/bandposttype/service/impl/BandPostTypeManageServiceImpl.java
+++ src/main/java/kr/aling/post/bandposttype/service/impl/BandPostTypeManageServiceImpl.java
@@ -1,8 +1,12 @@
 package kr.aling.post.bandposttype.service.impl;
 
+import kr.aling.post.bandpost.repository.BandPostReadRepository;
 import kr.aling.post.bandposttype.dto.request.CreateBandPostTypeRequestDto;
+import kr.aling.post.bandposttype.dto.request.ModifyBandPostTypeRequestDto;
 import kr.aling.post.bandposttype.entity.BandPostType;
 import kr.aling.post.bandposttype.exception.BandPostTypeAlreadyExistsException;
+import kr.aling.post.bandposttype.exception.BandPostTypeDeniedException;
+import kr.aling.post.bandposttype.exception.BandPostTypeNotFoundException;
 import kr.aling.post.bandposttype.repository.BandPostTypeManageRepository;
 import kr.aling.post.bandposttype.repository.BandPostTypeReadRepository;
 import kr.aling.post.bandposttype.service.BandPostTypeManageService;
@@ -21,6 +25,7 @@ public class BandPostTypeManageServiceImpl implements BandPostTypeManageService 
 
     private final BandPostTypeManageRepository bandPostTypeManageRepository;
     private final BandPostTypeReadRepository bandPostTypeReadRepository;
+    private final BandPostReadRepository bandPostReadRepository;
 
     /**
      * {@inheritDoc}
@@ -40,5 +45,45 @@ public class BandPostTypeManageServiceImpl implements BandPostTypeManageService 
                         .bandNo(requestDto.getBandNo())
                         .name(requestDto.getBandPostTypeName())
                         .build());
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param postTypeNo 수정할 그룹 게시글 분류 번호
+     * @param requestDto 수정할 그룹 게시글 분류 정보 dto
+     * @throws BandPostTypeAlreadyExistsException 그룹 내에 그룹 게시글 분류 명이 이미 존재하는 경우 발생 exception
+     * @throws BandPostTypeNotFoundException      그룹 게시글 분류를 찾을 수 없을 때 발생 exception
+     */
+    @Override
+    public void updateBandPostType(Long postTypeNo, ModifyBandPostTypeRequestDto requestDto) {
+        if (bandPostTypeReadRepository.existsByNameAndBandNo(requestDto.getBandPostTypeName(),
+                requestDto.getBandNo())) {
+            throw new BandPostTypeAlreadyExistsException();
+        }
+
+        BandPostType bandPostType = bandPostTypeReadRepository.findById(postTypeNo)
+                .orElseThrow(BandPostTypeNotFoundException::new);
+
+        bandPostType.updatePostType(requestDto.getBandPostTypeName());
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param postTypeNo 그룹 게시글 분류 번호
+     * @throws BandPostTypeNotFoundException 그룹 게시글 분류를 찾을 수 없을 때 발생 exception
+     * @throws BandPostTypeDeniedException   그룹 게시글 분류 내 삭제 되지 않은 그룹 게시글이 존재 하는 경우 삭제 불가능 exception
+     */
+    @Override
+    public void deleteBandPostType(Long postTypeNo) {
+        BandPostType bandPostType =
+                bandPostTypeReadRepository.findById(postTypeNo).orElseThrow(BandPostTypeNotFoundException::new);
+
+        if (bandPostReadRepository.getCountBandPostByBandPostTypeNo(postTypeNo) > 0) {
+            throw new BandPostTypeDeniedException();
+        }
+
+        bandPostType.deletePostType();
     }
 }
